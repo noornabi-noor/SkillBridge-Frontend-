6588c1037b50b2a75d0abda5a586d1c0f37db6ce
Noornabi <noornabimd9891@gmail.com>
2026-02-09 20:41:35 +0600

    Updated fetch with cookies part

--- src/services/fetchWithCookies.ts
+++ src/services/fetchWithCookies.ts
@@ -1,10 +1,13 @@
 "use server";
 import { cookies } from "next/headers";
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL!;
+const APP_URL = process.env.NEXT_PUBLIC_APP_URL!;
+
 export async function fetchWithCookies(url: string, options: RequestInit = {}) {
   const cookieStore = await cookies();
   const cookieHeader = cookieStore.getAll().map(c => `${c.name}=${c.value}`).join("; ");
-  const headers = { ...options.headers, Cookie: cookieHeader, Origin: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000" };
+  const headers = { ...options.headers, Cookie: cookieHeader, Origin: APP_URL };
 
   const res = await fetch(url, { ...options, headers, cache: "no-store" });
   return res;
